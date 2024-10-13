0a5d4dfef17ba65c8c818129c5a34907f7a1f632
Azzaz <hello@azzaz.dev>
2024-10-13 15:18:52 +0500

    feat: Added Laravel optimizations in entrypoint script 🎯

--- entrypoint.d/99-custom.sh
+++ entrypoint.d/99-custom.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Base Laravel optimizations
+php "$APP_BASE_DIR/artisan" config:clear >/dev/null 2>&2
+php "$APP_BASE_DIR/artisan" migrate --force >/dev/null 2>&2
+php "$APP_BASE_DIR/artisan" storage:link --force >/dev/null 2>&2
+php "$APP_BASE_DIR/artisan" optimize >/dev/null 2>&2
+
 # Write your code here ...
 
-echo "✅ Custom initilization script completed successfully"
+echo "✅ Laravel optimzations script completed"
